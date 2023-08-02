8f7b9b2921872bf1a395665dc9b1947ecf271a3f
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-08-02 21:10:51 +0300

    Done task2

--- Program.cs
+++ Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-
+            Shop shop1 = new Shop();
+            shop1.Input();
+            Console.WriteLine(shop1.ToString());
+            //operator +
+            Console.WriteLine("Enter sguare for operator +: ");
+            double sguare = Convert.ToDouble(Console.ReadLine());
+            shop1 += sguare;
+            Console.WriteLine(shop1.ToString());
+            //operator -
+            Console.WriteLine("Enter sguare for operator -: ");
+            sguare = Convert.ToDouble(Console.ReadLine());
+            shop1 -= sguare;
+            Console.WriteLine(shop1.ToString());
+            //operator ==
+            Shop shop2 = new Shop();
+            shop2.Input();
+            Console.WriteLine(shop2.ToString());
+            Console.WriteLine($"Operator == : {shop1 == shop2}");
+            //operator !=
+            Console.WriteLine($"Operator != : {shop1 != shop2}");
+            //operator >
+            Console.WriteLine($"Operator > : {shop1 > shop2}");
+            //operator <
+            Console.WriteLine($"Operator < : {shop1 < shop2}");
+            //Equals
+            Console.WriteLine($"Equals : {shop1.Equals(shop2)}");
         }
     }
 }
---
+++ Shop.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_HommeWork5
+{
+    public class Shop
+    {
+        public string Name { get; set; }
+        public double Sguare { get; set; }
+        public int CountOfEmployers { get; set; }
+
+        public Shop() {}
+        public Shop(string name, double sguare, int countOfEmployers)
+        {
+            Name = name;
+            Sguare = sguare;
+            CountOfEmployers = countOfEmployers;
+        }
+        public void Input()
+        {
+            Console.WriteLine("Enter name of shop: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter sguare of shop: ");
+            Sguare = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter count of employers: ");
+            CountOfEmployers = Convert.ToInt32(Console.ReadLine());
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Sguare: {Sguare}, Count of employers: {CountOfEmployers}";
+        }
+        public static Shop operator +(Shop shop, double sguare)
+        {
+            shop.Sguare += sguare;
+            return shop;
+        }
+        public static Shop operator -(Shop shop, double sguare)
+        {
+            shop.Sguare -= sguare;
+            return shop;
+        }
+        public static bool operator ==(Shop shop1, Shop shop2)
+        {
+            return shop1.Sguare == shop2.Sguare;
+        }
+        public static bool operator !=(Shop shop1, Shop shop2)
+        {
+            return shop1.Sguare != shop2.Sguare;
+        }
+        public static bool operator >(Shop shop1, Shop shop2)
+        {
+            return shop1.Sguare > shop2.Sguare;
+        }
+        public static bool operator <(Shop shop1, Shop shop2)
+        {
+            return shop1.Sguare < shop2.Sguare;
+        }
+        public override bool Equals(object obj)
+        {
+            return obj is Shop shop &&
+                Name == shop.Name &&
+                Sguare == shop.Sguare &&
+                CountOfEmployers == shop.CountOfEmployers;
+        }
+    }
+}
