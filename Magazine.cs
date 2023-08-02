8332de919449a2f8ae7038c54e19e57e28c7f440
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-08-02 20:54:17 +0300

    Done task1

---
+++ Magazine.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_HommeWork5
+{
+    public class Magazine
+    {
+        public string Name { get; set; }
+        public int NumberOfEmployees { get; set; }
+
+        public Magazine() {}
+        Magazine(string name, int numberOfEmployees)
+        {
+            Name = name;
+            NumberOfEmployees = numberOfEmployees;
+        }
+        public void Input()
+        {
+            Console.WriteLine("Enter name of magazine: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Enter number of employees of magazine: ");
+            NumberOfEmployees = Convert.ToInt32(Console.ReadLine());
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Number of employees: {NumberOfEmployees}";
+        }
+        public static Magazine operator +(Magazine magazine, int numberOfEmployees)
+        {
+            magazine.NumberOfEmployees += numberOfEmployees;
+            return magazine;
+        }
+        public static Magazine operator -(Magazine magazine, int numberOfEmployees)
+        {
+            magazine.NumberOfEmployees -= numberOfEmployees;
+            return magazine;
+        }
+        public static bool operator ==(Magazine magazine1, Magazine magazine2)
+        {
+            return magazine1.NumberOfEmployees == magazine2.NumberOfEmployees;
+        }
+        public static bool operator !=(Magazine magazine1, Magazine magazine2)
+        {
+            return magazine1.NumberOfEmployees != magazine2.NumberOfEmployees;
+        }
+        public static bool operator >(Magazine magazine1, Magazine magazine2)
+        {
+            return magazine1.NumberOfEmployees > magazine2.NumberOfEmployees;
+        }
+        public static bool operator <(Magazine magazine1, Magazine magazine2)
+        {
+            return magazine1.NumberOfEmployees < magazine2.NumberOfEmployees;
+        }
+        public override bool Equals(object obj)
+        {
+            return obj is Magazine magazine &&
+                   Name == magazine.Name &&
+                   NumberOfEmployees == magazine.NumberOfEmployees;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,33 @@
     {
         static void Main(string[] args)
         {
+            Magazine magazine = new Magazine();
+            magazine.Input();
+            Console.WriteLine(magazine.ToString());
+            //operator +
+            Console.WriteLine("Enter number of employees to add: ");
+            int numberOfEmployees = Convert.ToInt32(Console.ReadLine());
+            magazine += numberOfEmployees;
+            Console.WriteLine(magazine.ToString());
+            //operator -
+            Console.WriteLine("Enter number of employees to remove: ");
+            numberOfEmployees = Convert.ToInt32(Console.ReadLine());
+            magazine -= numberOfEmployees;
+            Console.WriteLine(magazine.ToString());
+
+            Magazine magazine1 = new Magazine();
+            magazine1.Input();
+            Console.WriteLine(magazine1.ToString());
+            //operator ==
+            Console.WriteLine($"Operator == : {magazine == magazine1}");
+            //operator !=
+            Console.WriteLine($"Operator != : {magazine != magazine1}");
+            //operator <
+            Console.WriteLine($"Operator < : {magazine < magazine1}");
+            //operator >
+            Console.WriteLine($"Operator > : {magazine > magazine1}");
+            //operator Equals
+            Console.WriteLine($"Operator Equals : {magazine.Equals(magazine1)}");
 
         }
     }
